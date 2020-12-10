2820684c2862ce1550339290956e257277bc4e8d
saikeerthi555 <saikeerthi.jupally@gmail.com>
2020-12-10 13:34:56 +0530

    added cart page

--- server.js
+++ server.js
@@ -10,22 +10,28 @@ const app = express();
 //Assets
 app.use(express.static('public'));
 
-app.get("/", function(req,res)
-{
-  res.render('home');
-});
 
 //set Template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
+app.get("/", function(req,res)
+{
+  res.render('home');
+});
 
+app.get("/cart",function(req,res){
+  res.render('customers/cart');
+});
 
+app.get("/login",function(req,res){
+  res.render('auth/login');
+});
 
-
-
-
+app.get("/register",function(req,res){
+  res.render('auth/register');
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
