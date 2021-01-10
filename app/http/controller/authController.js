fe05a5e64c75327bbcec33452393df7f2a829f2f
saikeerthi <saikeerthi.jupally@gmail.com>
2021-01-10 13:24:53 +0530

    added login error msg in auth controller

--- app/http/controller/authController.js
+++ app/http/controller/authController.js
@@ -12,6 +12,16 @@ function authController() {
     },
 
     postLogin(req, res, next){
+      const { email, password } = req.body
+
+      //validate request
+      if(!email || !password)
+      {
+        req.flash("error" , "All fields are required");
+        return res.redirect("/login");
+      }
+
+
     passport.authenticate("local", function(err, user, info){
       if(err) {
         req.flash("error", info.message)
